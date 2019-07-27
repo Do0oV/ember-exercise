05fb2d567adf9e0e40519c0447251af2535c07d7
dorothee <viard.dorothee@gmail.com>
2019-07-27 15:26:24 +0200

    feat: use mirage to get items

--- mirage/config.js
+++ mirage/config.js
@@ -1,21 +1,35 @@
 export default function() {
   this.namespace = '/api';
 
-  this.get('/shop', function() {
-    return {
-      data: [{
-        id: 'GR1',
-        name: 'Green tea',
-        price: 3.11
-      }, {
-        id: 'SR1',
-        name: 'Strawberries',
-        price: 5.00
-      }, {
-        id: 'CF1',
-        name: 'Coffee',
-        price: 11.23
-      }]
-    };
+  const items = [
+  {
+    id: '1',
+    type: 'item',
+    attributes: {
+      itemId: 'GR1',
+      name: 'Green tea',
+      price: 3.11
+    }
+  }, {
+    id: '2',
+    type: 'item',
+    attributes: {
+      itemId: 'SR1',
+      name: 'Strawberries',
+      price: 5.00
+    }
+  }, {
+    id: '3',
+    type: 'item',
+    attributes: {
+      itemId: 'CF1',
+      name: 'Coffee',
+      price: 11.23
+    }
+
+  }];
+
+  this.get('/items', function() {
+    return { data: items };
   });
 }
