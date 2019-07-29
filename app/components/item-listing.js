e071dd43c318c2b64b45b12864306fae8e50ff50
dorothee <viard.dorothee@gmail.com>
2019-07-29 09:12:30 +0200

    feat: update shop and items

--- app/components/item-listing.js
+++ app/components/item-listing.js
@@ -1,16 +1,22 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  cart: service('shopping-cart'),
   quantity: 1,
   actions: {
     addToCart(item, quantity) {
-      console.log(item.code)
-      console.log(quantity)
-      //this.get('shoppingCart').add(item);
+      let oldQ = 0;
+      this.cart.get('cartItems').forEach(el => {
+        if(el.code === item.code) {
+          oldQ = parseInt(el.quantity);
+        }
+      })
+      item.set('quantity', parseInt(quantity) + parseInt(oldQ))
+      this.cart.add(item);
     },
     updateQuantity(val) {
       this.set('quantity', val);
-      console.log(this.quantity)
     }
   }
 });
